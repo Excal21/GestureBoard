2620191c145275fd87fdd4f24e02c50e283d540a
Szakál Gyula Richárd <szgyuszi20@gmail.com>
2025-08-22 11:27:41 +0200

    Tesztlefedettség bug javítva
    Mostmár rendesen írja, hogy melyik fájlok mennyire lefedettek, valamint kihagyja a tesztfájlokat a reportból.

---
+++ Test/test_ImportEverything.py
@@ -0,0 +1,5 @@
+from UI.Controllers import CameraOptionsController, MainMenuController, NewGestureWizardController, OptionsMenuController, TrainMenuController, LoadingScreenController
+from UI.Models import MediaPipeHandler, Recognizer, RecognizerHandler, Recorder, Trainer
+
+def test_import_everything():
+    print('Controllers and Models imported')
