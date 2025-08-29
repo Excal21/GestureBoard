f28ee37b6e843429da46329f544bc60defd52497
Szakál Gyula Richárd <szgyuszi20@gmail.com>
2025-08-29 23:19:30 +0200

    Szerver bugfix

--- UI/Models/Trainer.py
+++ UI/Models/Trainer.py
@@ -6,57 +6,75 @@ import requests
 from PySide6.QtCore import QThread, Signal
 from Models.RecognizerHandler import RecognizerHandler
 
+CHUNK_SIZE = 30 * 1024 * 1024  # 30 MB
+
 class Trainer(QThread):
     progress = Signal(str)
 
     filename = 'Images.zip'
 
-
-
     def __init__(self):
         super().__init__()
-        self.ip = '127.0.0.1'
+        self.ip = '127.0.0.1:5000'
         self.filename = 'Images.zip'
         self.trained = False
     
     def run(self):
         try:
-            response = requests.get('http://' + self.ip + '/status', headers={'X-API-KEY': 'secret'})
+            response = requests.get('https://' + self.ip + '/status', headers={'X-API-KEY': 'secret'})
             if response.status_code == 200:
                 if response.json()['status'] == 'idle':
                     if os.path.exists(self.filename):
                         os.remove(self.filename)
                     make_archive('Images', 'zip', 'Data/Samples')
-                    with open(self.filename, 'rb') as file:
-                        files = {'file': file}
-                        response = requests.post('http://' + self.ip + '/upload', headers={'X-API-KEY' : 'secret'}, files=files)
-                    if(response.status_code == 200):
-                        self.progress.emit('Fájlok sikeresen feltöltve')
-                        #response = requests.get('http://' + self.ip + ':' + self.port + '/status', headers={'X-API-KEY': 'secret'})
-                        sleep(1)
-                        os.remove(self.filename)
-                        self.train()
+                    
+                    self.upload_in_chunks(self.filename)
+
+                    self.progress.emit('Fájlok sikeresen feltöltve')
+                    sleep(1)
+                    os.remove(self.filename)
+                    self.train()
                 else:
                     self.progress.emit('A kiszolgáló elfoglalt')
                     sleep(1)
                     self.finished.emit()
-
             else:
                 self.progress.emit('Kiszolgálóhiba')
-                print('Kiszolgálóhiba')
                 sleep(1)
                 self.finished.emit()
                 return
         except (requests.exceptions.ConnectionError , requests.exceptions.InvalidURL):
-            print('Kiszolgáló nem elérhető')
             self.progress.emit('Kiszolgáló nem elérhető')
             sleep(1)
             self.finished.emit()
             return
 
+    def upload_in_chunks(self, filepath):
+        file_size = os.path.getsize(filepath)
+        total_chunks = (file_size // CHUNK_SIZE) + (1 if file_size % CHUNK_SIZE else 0)
+
+        with open(filepath, "rb") as f:
+            for i in range(total_chunks):
+                chunk_data = f.read(CHUNK_SIZE)
+                files = {'file': (f"part_{i}", chunk_data)}
+                response = requests.post(
+                    'https://' + self.ip + '/upload_chunk',
+                    headers={'X-API-KEY': 'secret'},
+                    files=files
+                )
+                if response.status_code != 200:
+                    self.progress.emit(f"Hiba a {i}. szelet feltöltésekor")
+                    return
+        
+        response = requests.post('https://' + self.ip + '/merge_chunks',
+                                 headers={'X-API-KEY': 'secret'},
+                                 json={'filename': 'Images.zip'})
+        if response.status_code != 200:
+            self.progress.emit("Hiba a szeletek összefűzésekor")
+
     def train(self):
         while not self.trained:
-            response = requests.get('http://' + self.ip + '/status', headers={'X-API-KEY': 'secret'})
+            response = requests.get('https://' + self.ip + '/status', headers={'X-API-KEY': 'secret'})
             if response.json()['status'] == 'idle':
                 self.trained = True
                 self.progress.emit('Tanítás kész')
@@ -66,7 +84,7 @@ class Trainer(QThread):
                 sleep(1)
 
         if self.trained:
-            response = requests.get('http://' + self.ip + '/download',
+            response = requests.get('https://' + self.ip + '/download',
                         headers={'X-API-KEY': 'secret'})
             
             if response.status_code == 200:
@@ -75,8 +93,6 @@ class Trainer(QThread):
                 RecognizerHandler.getInstance().reload()
                 self.progress.emit('Modell elmentve')
                 self.trained = True
-                #self.finished.emit()
             else:
                 self.progress.emit('Hiba történt a modell letöltése közben')
                 sleep(1)
-                #self.finished.emit()
--- docker/server.py
+++ docker/server.py
@@ -9,43 +9,60 @@ app = Flask(__name__)
 training_state = {'status': 'idle'}
 t1 = None
 
-# API kulcs beállítása
-API_KEY = 'secret' #Ezt majd hashelni kell
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-# API-kulcs ellenőrzése
+API_KEY = 'secret'
+
 @app.before_request
 def check_auth():
     if request.headers.get('X-API-KEY') != API_KEY:
         return jsonify({'error': 'Unauthorized'}), 403
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    global t1
+@app.route('/upload_chunk', methods=['POST'])
+def upload_chunk():
     file = request.files['file']
-    file.save(file.filename)
-    if file and file.filename.endswith('.zip'):
-        shutil.unpack_archive(file.filename, 'Samples')
-    
+    save_path = os.path.join(UPLOAD_DIR, file.filename)
+    file.save(save_path)
+    return jsonify({'message': f'{file.filename} received'}), 200
+
+@app.route('/merge_chunks', methods=['POST'])
+def merge_chunks():
+    data = request.get_json()
+    final_filename = data.get('filename', 'Images.zip')
+    final_path = os.path.join(UPLOAD_DIR, final_filename)
+
+    with open(final_path, "wb") as outfile:
+        i = 0
+        while True:
+            part_path = os.path.join(UPLOAD_DIR, f"part_{i}")
+            if not os.path.exists(part_path):
+                break
+            with open(part_path, "rb") as infile:
+                shutil.copyfileobj(infile, outfile)
+            i += 1
+
+    if final_filename.endswith('.zip'):
+        shutil.unpack_archive(final_path, 'Samples')
+
+    global t1
     t1 = threading.Thread(target=ModelTrainer.train)
     t1.start()
     training_state['status'] = 'busy'
 
-    return jsonify({'message': 'File uploaded successfully'}), 200
+    return jsonify({'message': 'Chunks merged and training started'}), 200
 
 @app.route('/status', methods=['GET'])
 def status():
     if t1 is not None and t1.is_alive():
         training_state['status'] = 'busy'
-        print('busy')
     elif t1 is not None and not t1.is_alive() and training_state['status'] == 'busy':
         training_state['status'] = 'idle'
-        print('idle')
     return jsonify(training_state)
 
 @app.route('/download', methods=['GET'])
 def download():
     return send_file('gesture_recognizer.task', as_attachment=True)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
